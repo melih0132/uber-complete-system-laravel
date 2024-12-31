24db9fdf75cc8f36746ff17471c908b9c689eaac
melih0132 <melih.cetinkaya.32@gmail.com>
2024-12-31 02:18:46 +0100

    31 DEC 2024

---
+++ TRAVAUX/Uber/resources/views/layouts/ubereats.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@400;700&family=Segoe+UI&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/ubereat.blade.css') }}">
+    @yield('css')
+    <script src="{{ asset('js/main.js') }}"></script>
+    <link rel="icon" type="image/png" href="{{ asset('img/UberEatsPetit.png') }}">
+</head>
+
+<body>
+    <nav data-baseweb="header-navigation" role="navigation" class="nav-uber">
+        <div class="d-flex align-items-center w-100 justify-content-between">
+            <ul class="px-4">
+                <li>
+                    <a data-baseweb="link" href="{{ url('./UberEats') }}" target="_self" class="header-links">
+                        <img src="/img/UberEats.png" alt="Uber Eats" class="logo-image">
+                    </a>
+                </li>
+            </ul>
+            <ul class="ul-links">
+                <div class="d-flex justify-content-center align-items-center">
+                    <li class="mx-3">
+                        <a href="{{ url('/panier') }}">
+                            <i class="fas fa-cart-shopping panier"></i>
+                        </a>
+                    </li>
+                    <li class="li-links mx-2">
+                        <a class="a-login" href="{{ url('/UberEats/guide') }}">Aide</a>
+                    </li>
+                    @php
+                        $user = session('user');
+                    @endphp
+                    @if ($user)
+                        <li class="li-links mx-2">
+                            <a class="a-login" href="{{ url('/mon-compte') }}">Mon Compte</a>
+                        </li>
+                        <li class="css-fyrSIO mx-2">
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <button class="a-register" type="submit">Se déconnecter</button>
+                            </form>
+                        </li>
+                    @else
+                        <li class="li-links mx-2">
+                            <a class="a-login" href="{{ url('/interface-connexion') }}">Connexion</a>
+                        </li>
+                        <li class="css-fyrSIO mx-2">
+                            <a class="a-register" href="{{ url('/interface-inscription') }}">Inscription</a>
+                        </li>
+                    @endif
+                </div>
+            </ul>
+        </div>
+    </nav>
+
+    @yield('content')
+
+    <footer class="footer-container">
+        <div class="footer-links">
+            <a href="{{ url('/Cookies') }}">Politique de Confidentialité</a>
+        </div>
+        <div class="footer-info">
+            <p>&copy; {{ date('Y') }} Uber Eats. Tous droits réservés.</p>
+        </div>
+    </footer>
+
+    @yield('js')
+</body>
+
+</html>
---
+++ TRAVAUX/Uber/routes/web.php
@@ -0,0 +1,284 @@
+<?php
+
+use App\Http\Controllers\TranslationController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CoursierController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\CommandeController;
+use App\Http\Controllers\EtablissementController;
+use App\Http\Controllers\PanierController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\FactureController;
+use App\Http\Controllers\CarteBancaireController;
+use App\Http\Controllers\EntretienController;
+use App\Http\Controllers\LogistiqueController;
+use App\Http\Controllers\ProductController;
+use App\Models\Categorie_prestation;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// ! UBER
+Route::get('/', function () {
+    return view('accueil');
+});
+
+// Gestion des courses
+Route::post('/course', [CourseController::class, 'index'])->name('course.index');
+
+// 1 - Avant et pendant la course
+Route::post('/course/details', [CourseController::class, 'detailCourse'])->name('course.details');
+Route::post('/course/validate', [CourseController::class, 'courseAdd'])->name('course.validate');
+Route::post('/course/cancel', [CourseController::class, 'endCourse'])->name('course.cancel');
+
+// 2 - Après la course
+Route::post('/course/add-tip-rate', [CourseController::class, 'addTipAndRate'])->name('course.addTipRate');
+Route::post('/invoice/reservation/{idreservation}', [FactureController::class, 'index'])->name('invoice.view');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ! Uber Eats
+Route::get('/UberEats', [EtablissementController::class, 'accueilubereats'])->name('etablissement.accueilubereats');
+Route::get('/UberEats/etablissements', [EtablissementController::class, 'index'])->name('etablissement.index');
+Route::get('/UberEats/etablissements/{idetablissement}', [EtablissementController::class, 'detail'])->name('etablissement.detail');
+
+Route::get('/panier', [PanierController::class, 'index'])->name('panier.index');
+Route::post('/panier/ajouter', [PanierController::class, 'ajouterAuPanier'])->name('panier.ajouter');
+Route::put('/panier/mettre-a-jour/{idproduit}', [PanierController::class, 'mettreAJour'])->name('panier.mettreAJour');
+Route::delete('/panier/supprimer/{idProduit}', [PanierController::class, 'supprimerDuPanier'])->name('panier.supprimer');
+Route::post('/panier/vider', [PanierController::class, 'viderPanier'])->name('panier.vider');
+
+// Commander -> nécessite la connexion
+Route::post('/panier/commander', [PanierController::class, 'passerCommande'])->name('panier.commander');
+Route::get('/commande/{idcommande}', [CommandeController::class, 'show'])->name('commande.show');
+
+Route::get('/UberEats/addRestaurant', [EtablissementController::class, 'add'])->name('etablissement.add');
+Route::post('/UberEats/addRestaurant', [EtablissementController::class, 'store'])->name('etablissement.store');
+// ajout bannière etablissement
+Route::get('/UberEats/etablissement/{id}/add-banner', [EtablissementController::class, 'addBanner'])->name('etablissement.addBanner');
+Route::post('/UberEats/store-etablissement-banner', [EtablissementController::class, 'storeBanner'])->name('store.etablissement.banner');
+
+//ajout produit pour les nouveaux etablissements
+Route::get('/addProduct', [ProductController::class, 'create'])->name('produit.create');
+Route::post('/addProduct', [ProductController::class, 'store'])->name('produit.store');
+
+
+
+
+
+
+// ! Uber Velo
+Route::get('/UberVelo', function () {
+    return view('uber-velo');
+});
+
+
+
+
+
+
+
+
+
+
+// * POV COURSIER
+// Entretien RH
+Route::get('/coursier/entretien', [CoursierController::class, 'entretien'])->name('coursier.entretien');
+Route::post('/coursier/entretien/valider/{entretien}', [CoursierController::class, 'validerEntretien'])->name('coursier.entretien.valider');
+Route::post('/coursier/entretien/annuler/{entretien}', [CoursierController::class, 'annulerEntretien'])->name('coursier.entretien.annuler');
+Route::get('/coursier/entretien/planifie', [CoursierController::class, 'planifie'])->name('coursier.entretien.planifie');
+
+// Entretien Logistque
+Route::get('/conducteurs/demandes/{id}', [LogistiqueController::class, 'afficherDemandesParCoursier'])->name('conducteurs.demandes');
+Route::post('/vehicules/{id}/complete-modification', [LogistiqueController::class, 'markModificationAsCompleted'])->name('vehicules.completeModification');
+
+// Diff Type Coursier
+Route::get('/coursier/courses', [CoursierController::class, 'index'])->name('coursier.courses.index');
+Route::get('/coursier/livraisons', [CoursierController::class, 'index'])->name('coursier.livraisons.index');
+
+// Coursiers VTC
+Route::post('/coursier/courses/accept/{idreservation}', [CoursierController::class, 'acceptTask'])->name('coursier.courses.accept');
+Route::post('/coursier/courses/cancel/{idreservation}', [CoursierController::class, 'cancelTask'])->name('coursier.courses.cancel');
+Route::post('/coursier/courses/finish/{idreservation}', [CoursierController::class, 'finishTask'])->name('coursier.courses.finish');
+
+// Coursiers Uber Eats
+Route::post('/coursier/livraisons/accept/{idreservation}', [CoursierController::class, 'acceptTask'])->name('coursier.livraisons.accept');
+Route::post('/coursier/livraisons/cancel/{idreservation}', [CoursierController::class, 'cancelTask'])->name('coursier.livraisons.cancel');
+Route::post('/coursier/livraisons/finish/{idreservation}', [CoursierController::class, 'finishTask'])->name('coursier.livraisons.finish');
+
+
+
+
+
+
+
+
+// ! Login
+Route::get('/interface-connexion', function () {
+    return view('interface-connexion');
+});
+
+Route::get('/login', function () {
+    return view('auth/login');
+});
+Route::get('/login-driver', function () {
+    return view('auth/login-driver');
+});
+Route::get('/login-manager', function () {
+    return view('auth/login-manager');
+});
+Route::get('/login-service', function () {
+    return view('auth/login-service');
+});
+
+Route::post('/login', [LoginController::class, 'auth'])->name('auth');
+Route::get('/mon-compte', [LoginController::class, 'monCompte'])->name('mon-compte');
+Route::post('/update-profile-image', [LoginController::class, 'updateProfileImage'])->name('update.profile.image');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Carte Bancaire
+Route::get('/carte-bancaire', [CarteBancaireController::class, 'index'])->name('carte-bancaire.index');
+Route::get('/carte-bancaire/create', [CarteBancaireController::class, 'create'])->name('carte-bancaire.create');
+Route::post('/carte-bancaire', [CarteBancaireController::class, 'store'])->name('carte-bancaire.store');
+
+// Départ Favoris
+Route::get('/favoris', function () {
+    return view('favoris');
+});
+
+
+
+
+// ! Register
+Route::get('/interface-inscription', function () {
+    return view('interface-inscription');
+});
+
+Route::get('/register/driver', [RegisterController::class, 'showDriverRegistrationForm'])->name('register.driver');
+Route::get('/register/passenger', [RegisterController::class, 'showPassengerRegistrationForm'])->name('register.passenger');
+Route::get('/register/eats', [RegisterController::class, 'showEatsRegistrationForm'])->name('register.eats');
+Route::get('/register/manager', [RegisterController::class, 'showManagerRegistrationForm'])->name('register.manager');
+
+Route::post('/register/driver/form', [RegisterController::class, 'register'])->name('register');
+Route::post('/register/passenger/form', [RegisterController::class, 'register'])->name('register');
+Route::post('/register/eats/form', [RegisterController::class, 'register'])->name('register');
+Route::post('/register/manager/form', [RegisterController::class, 'register'])->name('register');
+
+
+
+
+
+
+
+
+
+// ! SERVICE UBER
+// RH
+Route::get('/entretiens/en-attente', [EntretienController::class, 'index'])->name('entretiens.index');
+Route::get('/entretiens/plannifies', [EntretienController::class, 'listePlannifies'])->name('entretiens.plannifies');
+Route::get('/entretiens/termines', [EntretienController::class, 'listeTermines'])->name('entretiens.termines');
+
+Route::get('/entretiens/planifier/{id?}', [EntretienController::class, 'showPlanifierForm'])->name('entretiens.planifierForm');
+Route::post('/entretiens/planifier/{id?}', [EntretienController::class, 'planifier'])->name('entretiens.planifier');
+
+Route::post('/entretiens/resultat/{id}', [EntretienController::class, 'enregistrerResultat'])->name('entretiens.resultat');
+Route::delete('/entretiens/supprimer/{id}', [EntretienController::class, 'supprimer'])->name('entretiens.supprimer');
+
+Route::post('/entretiens/{id}/valider', [EntretienController::class, 'validerCoursier'])->name('entretiens.validerCoursier');
+Route::post('/entretiens/{id}/refuser', [EntretienController::class, 'refuserCoursier'])->name('entretiens.refuserCoursier');
+
+// Service Logistique
+Route::get('/logistique/vehicules/validation', [LogistiqueController::class, 'index'])->name('logistique.vehicules');
+
+Route::get('/logistique/vehicules/select-coursier', [LogistiqueController::class, 'selectCoursier'])->name('logistique.coursiers.select');
+Route::get('/logistique/vehicules/create', [LogistiqueController::class, 'showAddVehiculeForm'])->name('logistique.vehicules.create');
+Route::post('/logistique/vehicules/store', [LogistiqueController::class, 'storeVehicule'])->name('logistique.vehicules.store');
+
+Route::post('/vehicules/{id}/valider', [LogistiqueController::class, 'valider'])->name('logistique.vehicules.valider');
+Route::post('/vehicules/{id}/refuser', [LogistiqueController::class, 'refuser'])->name('logistique.vehicules.refuser');
+
+Route::get('/logistique/vehicules/modifier/{id}', [LogistiqueController::class, 'showModifierForm'])->name('logistique.vehicules.modifierForm');
+Route::post('/logistique/vehicules/modifier/{id}', [LogistiqueController::class, 'demanderModification'])->name('logistique.vehicules.modifier');
+
+Route::get('/logistique/modifications', [LogistiqueController::class, 'afficherModifications'])->name('logistique.modifications');
+Route::delete('/modifications/{index}', [LogistiqueController::class, 'supprimerModification'])->name('modifications.supprimer');
+
+// Service Facturation
+Route::get('Uber/facturation', function () {
+    return view('coursier-courses');
+});
+
+Route::get('Uber/facturation/course', [CoursierController::class, 'filterTrips'])->name('coursier.courses.filter');
+
+
+
+
+
+// ! DROIT
+Route::get('/Cookies', function () {
+    return view('cookie-politique');
+});
+
+
+
+
+
+
+
+
+// ! GUIDE
+Route::get('Uber/guide', function () {
+    return view('aide-uber');
+});
+
+Route::get('/UberEats/guide', function () {
+    return view('aide-uber-eat');
+});
+
+Route::post('/translate', [TranslationController::class, 'translate']);
+
+
+
+
+
+
+
+
+// * ? * //
+// Route::post('/api/courses/decline', [CourseController::class, 'declineCourse']);
+// Route::get('/api/courses/declined', [CourseController::class, 'exportDeclinedCourses']);
+
+// Route::get('/accueil/UberEat', [AccueilUberEatController::class, 'index']);
+
+Route::get('/map', function () {
+    return view('map');
+});
+
+Route::post('/send-distance', [CourseController::class, 'receiveDistance']);
+
+/* Route::get('clients/create', [ClientController::class, 'create']);
+Route::post('clients', [ClientController::class, 'store']); */
